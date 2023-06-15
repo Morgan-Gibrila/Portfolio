0a7cb19cf42a148f8b5c191a00e24261ed62edb8
Morgan Gibrila <morgangibrila@hotmail.fr>
2023-06-15 22:59:48 +0200

    Bubble background

--- bubbleBackground.js
+++ bubbleBackground.js
@@ -1,83 +1,161 @@
-let Bubbles = [];
-let maxBubbles = 5;
+let bubbles = []; // Array for bubble object
+let colorPalette = []; // Array of color sets 
+let maxBubbles = 5; // Maximum amount of bubbles
+let mousePosition; // mouse possition 
+
 
 function setup(){
+    // --- Assigning canvas to div ---
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.parent("bubble_Background_container");
-    noStroke();
-    for ( let i = 0; i < maxBubbles; i++){
-        Bubbles.push(new Bubble());
-        Bubbles[i].inicialDirection();
+    
+    // creating 2D array for color sets
+    for ( let i = 0; i <5; i++){
+        colorPalette[i] = [];
     }
+
+    // --- Setting color pallets ---
+
+    // - First pallet - 
+    colorPalette[0][0] = color('#ff0000');
+    colorPalette[0][1] = color('#0000ff'); 
+    colorPalette[0][2] = color('#3cb371'); 
+    colorPalette[0][3] = color('#ee82ee'); 
+    colorPalette[0][4] = color('#ffa500'); 
+
+    // - Second pallet - 
+    colorPalette[1][0] = color('#1F74F2'); 
+    colorPalette[1][1] = color('#21B3FC'); 
+    colorPalette[1][2] = color('#29D8E6'); 
+    colorPalette[1][3] = color('#21FCD0'); 
+    colorPalette[1][4] = color('#1FF28B'); 
+
+    // - Third pallet - 
+    colorPalette[2][0] = color(''); 
+    colorPalette[2][1] = color(''); 
+    colorPalette[2][2] = color(''); 
+    colorPalette[2][3] = color(''); 
+    colorPalette[2][4] = color('');
+
+    // - Forth pallet - 
+    colorPalette[3][0] = color(''); 
+    colorPalette[3][1] = color(''); 
+    colorPalette[3][2] = color(''); 
+    colorPalette[3][3] = color(''); 
+    colorPalette[3][4] = color(''); 
+
+    // - Fifth pallet - 
+    colorPalette[4][0] = color(''); 
+    colorPalette[4][1] = color(''); 
+    colorPalette[4][2] = color(''); 
+    colorPalette[4][3] = color(''); 
+    colorPalette[4][4] = color('');
+
+    // - Selecting a random palette - 
+    let selectedPalette = Math.round(random(1));
+    console.log(selectedPalette);
+
+    // --- Instanciating bubbles & Setting each to one to a color from a pallet --- 
+    for ( let i = 0; i < maxBubbles; i++){
+        
+        bubbles.push(new Bubble());
+        bubbles[i].setColor(colorPalette[selectedPalette][i]);
+    }
+
+    noStroke();
+    mousePosition = createVector(mouseX,mouseY);
 }
 
 function draw(){
     background(255);
+    // --- Updating mouse possition vector --- 
+    updateMousePos(mouseX,mouseY);
 
-    for ( let i = 0; i < Bubbles.length; i++){
-        Bubbles[i].moveBubble();
-        Bubbles[i].updateBubble();
+    // --- Checking the moving diraction of each bubble & updating it
+    for ( let i = 0; i < bubbles.length; i++){
+        bubbles[i].moveBubble();
+        bubbles[i].updateBubble();
     }
 
+    // --- Creating a mouse cursor --- 
     fill(48,155,29);
-    ellipse(mouseX,mouseY,50);
+    ellipse(mousePosition.x, mousePosition.y, 50);
 }
 
+// --- Updating mouse possition vector function --- 
+function updateMousePos(x, y){
+    mousePosition.x = x;
+    mousePosition.y = y;
+}
+
+// --- Updating canvas size on window resize ---
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+// --- The object class ---
 class Bubble{
     constructor(){
-        this.posX = Math.random() * windowWidth;
-        this.posY = Math.random() * windowHeight;
-        this.increasX;
-        this.increasY;
-        this.startDirectionX = Math.random() * 2;
-        this.startDirectionY = Math.random() * 2;
-        this.speed = 2; 
-        this.bubbleSize = 200;
+        this.position = createVector(Math.random() * windowWidth, Math.random() * windowHeight);
+        this.velocity = p5.Vector.random2D();
+        this.velocity.mult(3);
+        this.bubbleRadius = 600;
         this.red = Math.random() * 256;
         this.green = Math.random() * 256;
         this.blue = Math.random() * 256;
+        this.color;
     }
 
-    inicialDirection(){
-        console.log(this.startDirectionX);
-        if (this.startDirectionX >= 1 ){
-            this.increasX = true;
-        }else if ( this.startDirectionX < 1 ){
-            this.increasX = false;
+    // - Checking if the bubbles should bouce off the canvcas walls -
+    moveBubble(){
+        
+        if (this.position.x <= (this.bubbleRadius/2)){
+            this.position.x = (this.bubbleRadius/2);
+            this.velocity.x *= -1;
+        } else if (this.position.x > windowWidth - (this.bubbleRadius/2) ){
+            this.position.x = windowWidth - (this.bubbleRadius/2);
+            this.velocity.x *= -1;
         }
-        if (this.startDirectionY >= 1 ){
-            this.increasY = true;
-        }else if ( this.startDirectionY < 0 ){
-            this.increasY = false;
-        }
-    }
 
-    moveBubble( ){
-        if (this.posX <= 0 ){
-            this.increasX = true;
-        } else if (this.posX > windowWidth){
-            this.increasX = false;
-        }
-        if (this.posY <= 0 ){
-            this.increasY = true;
-        } else if (this.posY > windowHeight){
-            this.increasY = false;
+        if (this.position.y <= (this.bubbleRadius/2) ){
+            this.position.y = (this.bubbleRadius/2);
+            this.velocity.y *= -1;
+        } else if (this.position.y > windowHeight - (this.bubbleRadius/2) ){
+            this.position.y = windowHeight - (this.bubbleRadius/2);
+            this.velocity.y *= -1;
         }
     
-        if (this.increasX){
-            this.posX = this.posX + this.speed;
-        } else {
-            this.posX = this.posX - this.speed;
-        }
-        if (this.increasY){
-            this.posY = this.posY + this.speed
-        } else {
-            this.posY = this.posY - this.speed;
-        }
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 
+    // - Setting one given color from a color pallet to an instance of this class - 
+    setColor(color){
+        this.color = color;
+    }
+
+    // - Visually updating the Object -
     updateBubble(){
-        fill(this.red, this.green, this.blue);
-        ellipse(this.posX, this.posY,this.bubbleSize);
+        fill(this.color);
+        ellipse(this.position.x, this.position.y, this.bubbleRadius);
     }
 }
+
+/* --- Nuke Zone ---
+
+// for ( let i = 0; i < bubbles.length; i++){
+    //     for ( let x = 0; x < bubbles.length; x++){
+    //     checkCollision(bubbles[i], bubbles[x]);
+    //     }
+    // }
+
+    // --- Checking for collison between bubbles --- 
+function checkCollision(object, other){
+    let distance = object.position.dist(other.position);
+    if (distance < object.bubbleRadius && distance != 0){
+        object.increasX = !object.increasX;
+        object.increasY = !object.increasY;
+        return;
+    }
+}
+*/
